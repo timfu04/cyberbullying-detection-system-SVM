65d3c13ea8b2410cb35be07109f706b9aa843892
Clement <clementlee1999@gmail.com>
2023-04-04 23:28:53 +0800

    added pip install in script

--- cyberbullying/__init__.py
+++ cyberbullying/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-import tkinter as tk
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '104fca1a510827c2d5e256ed0c1d689b'
@@ -11,5 +10,13 @@ db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
+
+
+import subprocess
+
+def install(name):
+    subprocess.call(['pip', 'install', "tk"])
+
+install()
 
 from cyberbullying import routes
